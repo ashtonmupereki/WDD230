8bb62f23ec9e7096bec9c28b10bb254e3fe70553
ashton mupereki <mup23001@byui.edu>
2023-09-22 05:34:00 +0200

    added

--- getdates.js
+++ getdates.js
@@ -1,11 +1,6 @@
 // Get the current year
 const currentYear = new Date().getFullYear();
 
-// Get the last modified date
-const lastModifiedDate = document.lastModified;
-
 // Update the copyright year in the first paragraph of the footer
 document.querySelector('footer p:first-child').innerHTML = `&copy; ${currentYear} Ashton, Gweru, Zimbabwe`;
 
-// Update the last modified date in the second paragraph of the footer
-document.getElementById('lastModified').innerHTML = `Last Modified: ${lastModifiedDate}`;

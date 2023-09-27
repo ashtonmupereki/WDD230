3dba6867e7e62b8d774838662dbd627042ca5a59
ashton mupereki <mup23001@byui.edu>
2023-09-27 18:41:50 +0200

    added

--- getdates.js
+++ getdates.js
@@ -4,21 +4,10 @@ const currentYear = new Date().getFullYear();
 // Update the copyright year in the first paragraph of the footer
 document.querySelector('footer p:first-child').innerHTML = `&copy; ${currentYear} Ashton, Gweru, Zimbabwe`;
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu')
 
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
+hambutton.addEventListener('click',()=>{
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show')
+});

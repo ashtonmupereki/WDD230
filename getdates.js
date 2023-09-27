d5412727c992e4b74b6180abc9f80a626b0d3b01
ashton mupereki <mup23001@byui.edu>
2023-09-27 14:55:57 +0200

    added

--- getdates.js
+++ getdates.js
@@ -4,3 +4,21 @@ const currentYear = new Date().getFullYear();
 // Update the copyright year in the first paragraph of the footer
 document.querySelector('footer p:first-child').innerHTML = `&copy; ${currentYear} Ashton, Gweru, Zimbabwe`;
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}

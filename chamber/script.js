ba2b87b77f7ed7849b9fa4d2813e564d34c7ac8a
ashton mupereki <mup23001@byui.edu>
2023-10-18 13:41:01 +0200

    added

--- chamber/script.js
+++ chamber/script.js
@@ -6,3 +6,15 @@ hambutton.addEventListener('click',()=>{
     hambutton.classList.toggle('show')
 });
 
+function darkMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+    content.innerText = "Dark Mode is ON";
+}
+function lightMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+    content.innerText = "Dark Mode is OFF";
+}

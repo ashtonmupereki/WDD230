24d50aa1790a57a80863b3031368380fc2edfad2
ashton mupereki <mup23001@byui.edu>
2023-10-26 12:38:09 +0200

    added

--- getdates.js
+++ getdates.js
@@ -11,3 +11,18 @@ hambutton.addEventListener('click',()=>{
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show')
 });
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `We are pleased with your first visit. Welcome!`;
+}
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
+
+

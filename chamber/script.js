f0b0b158c998d8888bee02dbc6e12b87976581fd
ashton mupereki <mup23001@byui.edu>
2023-10-29 14:21:08 +0200

    added

--- chamber/script.js
+++ chamber/script.js
@@ -18,3 +18,33 @@ function lightMode() {
     element.className = "light-mode";
     content.innerText = "Dark Mode is OFF";
 }
+
+// milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
+const msToDays = 1000 * 60 * 60 * 24;
+
+// initialize display elements
+const todayElement = document.querySelector("#today");
+const daysElement = document.querySelector("#daysleft");
+
+// processing
+const today = Date.now();
+
+if (!localStorage.getItem("lastVisit")) {
+  daysElement.textContent = "Welcome! Let us know if you have any questions.";
+} else {
+  const lastVisit = parseInt(localStorage.getItem("lastVisit"));
+  const timeDifference = today - lastVisit;
+  const daysSinceLastVisit = Math.floor(timeDifference / msToDays);
+
+  if (daysSinceLastVisit < 1) {
+    daysElement.textContent = "Back so soon! Awesome!";
+  } else {
+    const message = (daysSinceLastVisit === 1) ? "day" : "days";
+    daysElement.textContent = `You last visited ${daysSinceLastVisit} ${message} ago.`;
+  }
+}
+
+todayElement.textContent = new Date(today).toLocaleString();
+
+// Store the current visit date in localStorage
+localStorage.setItem("lastVisit", today.toString());
